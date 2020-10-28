7131895c39edbc8ead00bf491e728b116a1decc6
Vladyslav <anisimov.vlad@gmail.com>
2020-10-28 17:39:10 +0200

    Fix retrofit instantiation

--- app/src/main/java/cc/anisimov/vlad/simplealbumlist/di/module/AppModule.kt
+++ app/src/main/java/cc/anisimov/vlad/simplealbumlist/di/module/AppModule.kt
@@ -9,6 +9,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -29,6 +30,9 @@ class AppModule {
     fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .addConverterFactory(
+                GsonConverterFactory.create()
+            )
             .build()
     }
 
