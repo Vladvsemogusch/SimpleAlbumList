06c7210d367738ef1f78c51ef2889fcd572bfd72
Vladyslav <anisimov.vlad@gmail.com>
2020-10-28 15:38:29 +0200

    Fix return type

--- app/src/main/java/cc/anisimov/vlad/simplealbumlist/data/source/remote/TypicodeApi.kt
+++ app/src/main/java/cc/anisimov/vlad/simplealbumlist/data/source/remote/TypicodeApi.kt
@@ -1,13 +1,16 @@
 package cc.anisimov.vlad.simplealbumlist.data.source.remote
 
+import cc.anisimov.vlad.simplealbumlist.data.model.Album
+import cc.anisimov.vlad.simplealbumlist.data.model.Photo
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TypicodeApi {
 
     @GET("albums")
-    fun getAlbumsByUserId(@Query("userId") userId: Int)
+    fun getAlbumsByUserId(@Query("userId") userId: Int): Response<List<Album>>
 
     @GET("photos")
-    fun getPhotosByAlbumId(@Query("albumId") albumId: Int)
+    fun getPhotosByAlbumId(@Query("albumId") albumId: Int): Response<List<Photo>>
 }
